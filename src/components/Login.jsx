fe9d32511d6be46817394e7d8441495d6e747571
Todd Albert <todd@bocacode.com>
2022-11-04 11:59:29 -0400

    added form handler and rules

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,14 +1,23 @@
 import { Button, Form, Input, Layout } from 'antd'
 
 export default function Login() {
+  const handleFormSubmit = (values) => {
+    console.log('Success:', values)
+  }
   return (
     <Layout.Content style={{ padding: '50px' }}>
       <h1>Login</h1>
-      <Form labelCol={{ span: 8 }} wrapperCol={{ span: 16 }}>
-        <Form.Item label="Email" name="email">
+      <Form onFinish={handleFormSubmit} labelCol={{ span: 8 }} wrapperCol={{ span: 16 }}>
+        <Form.Item label="Email" name="email" rules={[{
+          required: true,
+          message: 'Please enter a valid email address'
+        }]}>
           <Input />
         </Form.Item>
-        <Form.Item label="Password" name="password">
+        <Form.Item label="Password" name="password" rules={[{
+          required: true,
+          message: 'Please enter password'
+        }]}>
           <Input.Password />
         </Form.Item>
         <Form.Item wrapperCol={{ span: 16, offset: 8 }}>
